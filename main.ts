16ba60264dc15e955d52cc4ef514b284dfd020a2
Kaleb Ercanbrack <twitcherc@gmail.com>
2023-05-04 01:01:08 -0600

    remove runtime env  setting

--- main.ts
+++ main.ts
@@ -6,7 +6,4 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
-import { load } from "https://deno.land/std@0.176.0/dotenv/mod.ts";
-const env = await load();
-Deno.env.set("AUTH_PASSWORD", env.AUTH_PASSWORD);
 await start(manifest);
