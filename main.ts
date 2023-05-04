f7dee30f45102ba906483a9d56014f318c158fd8
Kaleb Ercanbrack <twitcherc@gmail.com>
2023-05-04 01:16:09 -0600

    bring back dotenv

--- main.ts
+++ main.ts
@@ -6,4 +6,6 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import { load } from "https://deno.land/std@0.185.0/dotenv/mod.ts";
+await load({ export: true });
 await start(manifest);
