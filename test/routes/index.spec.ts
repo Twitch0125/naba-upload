3dd1728824a83bf811192b96b67f1ef4403d24bd
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-05-19 17:51:56 -0600

    don't save tar file, just slows things down

--- test/routes/index.spec.ts
+++ test/routes/index.spec.ts
@@ -1,24 +1,21 @@
 import { fetch as fileFetch } from "file_fetch";
 import extractTar from "../../utils/extractTar.ts";
-import saveTar from "../../utils/saveTar.ts";
 import { exists } from "fs/exists.ts";
 import { assert } from "https://deno.land/std@0.96.0/_util/assert.ts";
 
 Deno.test(
-  "POST index route",
+  "POST route",
   { permissions: { read: true, write: true } },
   async (t) => {
     t.name = "handle file save";
     const url = Deno.cwd() + "/test/data/test-dir.tar.gz";
     const res = await fileFetch(import.meta.resolve(url));
     const file = new File([await res.arrayBuffer()], "test-dir.tar.gz");
-    const saveUrl = Deno.cwd() + "/test/data/test.tar.gz";
-    await saveTar(file, saveUrl);
-    await extractTar(saveUrl, Deno.cwd() + "/test/data/extracted");
-    assert(await exists(saveUrl));
+    const extractUrl = Deno.cwd() + "/test/data/extracted";
+    await extractTar(file, Deno.cwd() + "/test/data/extracted");
+    assert(await exists(extractUrl));
     await t.step("cleanup", async () => {
-      await Deno.remove(saveUrl);
-      await Deno.remove(Deno.cwd() + "/test/data/extracted", { recursive: true })
-    })
+      await Deno.remove(extractUrl, { recursive: true });
+    });
   },
 );
--- utils/extractTar.ts
+++ utils/extractTar.ts
@@ -2,23 +2,12 @@ import { Untar } from "archive/mod.ts";
 import { ensureFile } from "fs/mod.ts";
 import { copy } from "streams/copy.ts";
 import { readerFromStreamReader } from "streams/reader_from_stream_reader.ts";
-import { fetch as fileFetch } from "file_fetch/mod.ts";
-export default async function extractTar(path: string, to: string) {
+export default async function extractTar(file: File, to: string) {
   //decompress gzipped tar
-  const url = import.meta.resolve(path);
-  const res = await fileFetch(url);
-  if (!res.ok) {
-    throw new Error(
-      `Failed to fetch report file: ${res.status} ${res.statusText}`,
-    );
-  }
-  if (!res.body) throw new Error("No body");
-
-  const reader = res.body.pipeThrough<Uint8Array>(
+  const reader = file.stream().pipeThrough<Uint8Array>(
     new DecompressionStream("gzip"),
   )
     .getReader();
-
   //extract tar from path and save to extracted folder
   const untar = new Untar(readerFromStreamReader(reader));
 
@@ -27,8 +16,8 @@ export default async function extractTar(path: string, to: string) {
       await ensureFile(`${to}/${entry.fileName}`);
       const reader = entry;
       const writer = await Deno.create(`${to}/${entry.fileName}`);
-      await copy(reader, writer)
-      writer.close()
+      await copy(reader, writer);
+      writer.close();
     }
   }
   return `${to}/report.json`;
